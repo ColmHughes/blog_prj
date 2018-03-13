404d6f86f2bd6005e90ae0b496b7ab76c302f3b8
Colm Hughes <cjhugo1991@hotmail.com>
2018-03-13 22:48:35 +0000

    Blog basics

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,10 @@ from .views import *
 
 urlpatterns = [
     url(r'^$', blog_home, name='blog_home'),
-    url(r'^view_blog', view_blog, name='view_blog'),
+    url(r'^post_list', post_list, name='post_list'),
+    url(r'^post_detail/(\d+)', post_detail, name='post_detail'),
+    url(r'^new_post', new_post, name='new_post'),
+    url(r'^edit_post/(\d+)', edit_post, name='edit_post'),
+    
    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import NewPostForm, EditPostForm
 
 # Create your views here.
 def blog_home(request):
     return render(request, 'blog/index.html')
     
-def view_blog(request):
+def post_list(request):
     blogs = Post.objects.all()
-    return render(request, 'blog/view_blog.html', {'blogs': blogs})
+    return render(request, 'blog/post_list.html', {'blogs': blogs})
+    
+def post_detail(request, id):
+    post=get_object_or_404(Post, pk=id)
+    post.read=True
+    post.save
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+    
+def new_post(request):
+    if request.method=="POST":
+        form = NewPostForm(request.POST)
+        message = form.save(commit=False)
+        message.author = request.user
+        message.save()
+        return redirect('post_list')
+    else:
+        form = NewPostForm()
+    
+    return render(request, "blog/new_post.html", { 'form': form })
+    
+    
+    
+    
+    
+def edit_post(request, id):
+    item = get_object_or_404(Post, pk=id)
+    if request.method == "POST":
+        print("It's a post")
+        form = EditPostForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect("post_list")
+    else:
+        print("It's a get")
+    
+    form = EditPostForm(instance=item)
+    return render(request, 'blog/edit_post.html', {'form': form})
